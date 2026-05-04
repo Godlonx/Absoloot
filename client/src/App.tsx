bb254699c1aef473167b0ba297c2da12f4ba46ea
Maxence Mahieux <pro.maxencemahieux@gmail.com>
2026-05-04 11:38:54 +0200

    🚀 | Setup app routing with protected routes and auth provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,63 @@
 import { Routes, Route } from "react-router"
 import Home from "@/pages/Home"
 import Adventurers from "@/pages/Adventurers"
+import AdventurerDetail from "@/pages/AdventurerDetail"
+import AdventurerForm from "@/pages/AdventurerForm"
+import Competences from "@/pages/Competences"
+import CompetenceDetail from "@/pages/CompetenceDetail"
+import CompetenceForm from "@/pages/CompetenceForm"
 import Login from "@/pages/Login"
 import Register from "@/pages/Register"
+import NotFound from "@/pages/NotFound"
+import { ProtectedRoute } from "@/components/ProtectedRoute"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/adventurers" element={<Adventurers />} />
+      <Route path="/adventurers" element={
+        <ProtectedRoute>
+          <Adventurers />
+        </ProtectedRoute>
+      } />
+      <Route path="/adventurers/new" element={
+        <ProtectedRoute requiredRole="ADMIN">
+          <AdventurerForm />
+        </ProtectedRoute>
+      } />
+      <Route path="/adventurers/:id" element={
+        <ProtectedRoute>
+          <AdventurerDetail />
+        </ProtectedRoute>
+      } />
+      <Route path="/adventurers/:id/edit" element={
+        <ProtectedRoute requiredRole="ADMIN">
+          <AdventurerForm />
+        </ProtectedRoute>
+      } />
+      <Route path="/competences" element={
+        <ProtectedRoute>
+          <Competences />
+        </ProtectedRoute>
+      } />
+      <Route path="/competences/new" element={
+        <ProtectedRoute requiredRole="ADMIN">
+          <CompetenceForm />
+        </ProtectedRoute>
+      } />
+      <Route path="/competences/:id" element={
+        <ProtectedRoute>
+          <CompetenceDetail />
+        </ProtectedRoute>
+      } />
+      <Route path="/competences/:id/edit" element={
+        <ProtectedRoute requiredRole="ADMIN">
+          <CompetenceForm />
+        </ProtectedRoute>
+      } />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,16 +5,19 @@ import { BrowserRouter } from "react-router"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { AuthProvider } from "@/contexts/AuthContext.tsx"
 import GlobalLayout from "./layouts/GlobalLayout.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light">
-      <BrowserRouter>
-        <GlobalLayout>
-          <App />
-        </GlobalLayout>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <GlobalLayout>
+            <App />
+          </GlobalLayout>
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>
 )
